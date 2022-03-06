073de8f167d05923bc1b3a4cf8476e2050331cdf
VictoKu1 <kushnir.victor@gmail.com>
2022-03-06 17:47:24 +0200

    .

--- ProjectFrontEndThroughUntiy/Assets/Scripts/Manager.cs
+++ ProjectFrontEndThroughUntiy/Assets/Scripts/Manager.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using System.Net;
 public class Manager : MonoBehaviour
 {
     public void Send()
     {
-        string draft = "{\"Items\":" + MainControl.numberOfCases + ",\n\"Mandate\":" + mandatesString() + ",\n\"Preferences\":" + preferencesString() + "}";
+        string draft = "{\"items\":" + MainControl.numberOfCases + ",\n\"mandates\":" + mandatesString() + ",\n\"preferences\":" + preferencesString() + "}";
         MainControl.serverInput = draft;
-        SceneManager.LoadScene("CasesChoose");
+        //*SceneManager.LoadScene("CasesChoose");
+        Server();
+
     }
     public string mandatesString()
     {
@@ -29,11 +34,33 @@ public class Manager : MonoBehaviour
             preferences += "[";
             for (int j = 0; j < MainControl.partyParameters.GetLength(1); j++)
             {
-                preferences += MainControl.partyParameters[i, j]+""+((j == MainControl.partyParameters.GetLength(1)) ? "" : ",");
+                preferences += MainControl.partyParameters[i, j] + "" + ((j == MainControl.partyParameters.GetLength(1)-1) ? "" : ",");
             }
             preferences += "]" + ((i == MainControl.partyParameters.GetLength(0) - 1) ? "" : ",");
         }
         preferences += "]";
         return preferences;
+    }
+    public void Server()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:8000/api/");
+        request.Method = "POST";
+        // ... just sending a string
+        string data = MainControl.serverInput;
+        request.ContentType = "application/json";
+        request.ContentLength = data.Length;
+        StreamWriter streamWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+        streamWriter.Write(data);
+        streamWriter.Close();
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string strResponse = reader.ReadToEnd();
+            Debug.Log(strResponse);
+        }
+        response.Close();
     }
 }
