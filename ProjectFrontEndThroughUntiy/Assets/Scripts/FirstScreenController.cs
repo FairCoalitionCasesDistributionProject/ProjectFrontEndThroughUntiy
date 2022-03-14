7d2a49b4945dda67957dae97ed055d63937df949
VictoKu1 <kushnir.victor@gmail.com>
2022-03-14 22:01:43 +0200

    .

--- ProjectFrontEndThroughUntiy/Assets/Scripts/FirstScreenController.cs
+++ ProjectFrontEndThroughUntiy/Assets/Scripts/FirstScreenController.cs
@@ -1,17 +1,28 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
 public class FirstScreenController : MonoBehaviour
 {
     public Text key;
     public bool wasChanged = false;
+    public GameObject mainImage;
+    public GameObject loading;
     void Start()
     {
         MainControl.lastPage = "Screen1";
+        loading.SetActive(false);
     }
     public void LoadPartyChoose()
     {
@@ -19,11 +30,9 @@ public class FirstScreenController : MonoBehaviour
         {
             wasChanged = false;
             string input = baseConversator64To10(key.text);
-            MainControl.key = ((input == "") ? "" : input);
-            //* TOOD: Check if key is valid.
+            MainControl.key = input;
+            ReUse();
         }
-        MainControl.lastPage = "PartyChoose";
-        SceneManager.LoadScene("PartyChoose");
     }
     public void LoadInformation()
     {
@@ -46,7 +55,7 @@ public class FirstScreenController : MonoBehaviour
         for (int i = 0; i < array.Length; i++)
         {
             int deciNum = toDeci(array[i], 61);
-            if (deciNum != -1)
+            if (deciNum == -1)
             {
                 Debug.Log("Error: Invalid input.");
                 return "";
@@ -82,8 +91,7 @@ public class FirstScreenController : MonoBehaviour
         int len = str.Length;
         int power = 1;
         int num = 0;
-        int i;
-        for (i = len - 1; i >= 0; i--)
+        for (int i = len - 1; i >= 0; i--)
         {
             if (val(str[i]) >= toBase)
             {
@@ -94,11 +102,57 @@ public class FirstScreenController : MonoBehaviour
         }
         return num;
     }
-
-
+    public void ReUse()
+    {
+        StartCoroutine(Upload());
+    }
+    IEnumerator Upload()
+    {
+        string URL = "http://faircol.herokuapp.com/api/getsave";
+        string json = "{\"key\":\"" + MainControl.key + "\"}";
+        loading.SetActive(true);
+        var uwr = new UnityWebRequest(URL, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+        yield return uwr.SendWebRequest();
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+            MainControl.serverOutput = "" + (-1);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            MainControl.serverOutput = uwr.downloadHandler.text;
+        }
+        Parse(MainControl.serverOutput);
+        loading.SetActive(false);
+        MainControl.lastPage = "PartyChoose";
+        SceneManager.LoadScene("PartyChoose");
+    }
+    public void Parse(string input)
+    {
+        if (input == "-1")
+        {
+            //*TODO: What to do if the input is broken.
+            return;
+        }
+        var cleanedRows = Regex.Split(input.Replace("\"", "").Replace("[", "{").Replace("]", "}"), @"}\s*,\s*{").Select(r => r.Replace("{", "").Replace("}", "").Trim()).ToList();
+        int[,] matrix = new int[cleanedRows.Count, 30];
+        for (var i = 0; i < cleanedRows.Count; i++)
+        {
+            var data = cleanedRows.ElementAt(i).Split(',');
+            var matrixHelper = data.Select(c => int.Parse(c.Trim())).ToArray();
+            for (var j = 0; j < matrixHelper.Length; j++)
+            {
+                matrix[i, j] = matrixHelper[j];
+            }
+        }
+        MainControl.partyParameters = matrix;
+    }
 }
-
-
 
 
 
