b08e891f0854864a544db124f092470bc11c39b0
VictoKu1 <kushnir.victor@gmail.com>
2022-04-23 07:11:23 +0300

    .

---
+++ ProjectFrontEndThroughUntiy/Assets/Scripts/GlobalPartyChoose.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GlobalPartyChoose : MonoBehaviour
+{
+    public Text numberOfParties;
+    public Button confirm;
+    public GameObject alert;
+    public Text alertText;
+
+
+
+
+    void Start()
+    {
+        alert.SetActive(false);
+    }
+
+
+
+
+
+    public void confirmPressed()
+    {
+        if (numberOfParties.text == "" || !int.TryParse(numberOfParties.text, out int numberOfPartiesInt))
+        {
+            alertShow(true, "Please enter a number .", 1.5f);
+        }
+    }
+    public void alertShow(bool show, string message, float time)
+    {
+        alert.SetActive(show);
+        alertText.text = message;
+        StartCoroutine(hideAlert(time));
+    }
+    IEnumerator hideAlert(float time)
+    {
+        yield return new WaitForSeconds(time);
+        alert.SetActive(false);
+    }
+}
--- ProjectFrontEndThroughUntiy/Assets/Scripts/Welcome.cs
+++ ProjectFrontEndThroughUntiy/Assets/Scripts/Welcome.cs
@@ -15,59 +15,53 @@ using System.IO;
 using System.Threading.Tasks;
 public class Welcome : MonoBehaviour
 {
+    // public GameObject loading;
+    // public bool gotAnswer = false;
+    // void Start()
+    // {
+    //     string URL = "http://faircol.herokuapp.com/api/";
+    //     string json = "{WakeUpAndBeReady.}";
+    //     StartCoroutine(Upload(URL, json));
+    // }
+
+    // void Update()
+    // {
+    //     if (gotAnswer)
+    //     {
+    //         loading.SetActive(false);
+    //     }
+    // }
 
 
+    // IEnumerator Upload(string URL, string json)
+    // {
+    //     var uwr = new UnityWebRequest(URL, "POST");
+    //     byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+    //     uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+    //     uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+    //     uwr.SetRequestHeader("Content-Type", "application/json");
+    //     yield return uwr.SendWebRequest();
+    //     if (uwr.isNetworkError)
+    //     {
+    //         Debug.Log("Error While Sending: " + uwr.error);
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("OK! " + uwr.downloadHandler.text);
+    //     }
 
-
-
-
-
-
-    public GameObject loading;
-    public bool gotAnswer = false;
-    void Start()
-    {
-        string URL = "http://faircol.herokuapp.com/api/";
-        string json = "{WakeUpAndBeReady.}";
-        StartCoroutine(Upload(URL, json));
-    }
-
-    void Update()
-    {
-        if (gotAnswer)
-        {
-            loading.SetActive(false);
-        }
-    }
-
-
-    IEnumerator Upload(string URL, string json)
-    {
-        var uwr = new UnityWebRequest(URL, "POST");
-        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
-        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
-        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-        uwr.SetRequestHeader("Content-Type", "application/json");
-        yield return uwr.SendWebRequest();
-        if (uwr.isNetworkError)
-        {
-            Debug.Log("Error While Sending: " + uwr.error);
-        }
-        else
-        {
-            Debug.Log("OK! " + uwr.downloadHandler.text);
-        }
-
-        gotAnswer = true;
-    }
-
-
-
+    //     gotAnswer = true;
+    // }
     public void LoadIsraelMode1()
     {
-        if (gotAnswer)
-        {
-            SceneManager.LoadScene("Scene1");
-        }
+        SceneManager.LoadScene("Screen1");
+    }
+    public void LoadPopularMode()
+    {
+        SceneManager.LoadScene("Popular");
     }
 }
+
+
+
+
